21da1f8fb7d0df77ca02006d4f32770a88950331
Jasper <japsai@gmail.com>
2017-05-31 20:18:33 +0100

    Basic package structure and Travis config

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from datetime import datetime
+seconds = round((datetime.now() - datetime(2017, 5, 31)).total_seconds())
+
+
+requires = []
+with open('requirements.txt') as f:
+    requires = f.read().splitlines()
+
+setup(name='scrolls',
+      version='0.0.'+str(seconds),
+      description='Log manager.',
+      url='https://github.com/ilogue/scrolls',
+      long_description='',
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Jasper J.F. van den Bosch',
+      author_email='japsai@gmail.com',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      scripts=['exe/scrolls'],
+      test_suite="test",
+      entry_points="""\
+      [paste.app_factory]
+      main = webtasks:main
+      """,
+      )
