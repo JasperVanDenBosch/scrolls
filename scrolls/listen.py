95a9a3f628b57ac61343d46def4d7de5f94246c0
Jasper <japsai@gmail.com>
2017-06-01 00:07:05 +0100

    Simple listener

---
+++ scrolls/listen.py
@@ -0,0 +1,20 @@
+import socketserver
+
+
+class SyslogTCPHandler(socketserver.StreamRequestHandler):
+
+    def handle(self):
+        # self.rfile is a file-like object created by the handler;
+        # we can now use e.g. readline() instead of raw recv() calls
+        self.data = self.rfile.readline().strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+
+if __name__ == "__main__":
+    try:
+        server = socketserver.TCPServer(("0.0.0.0", 514), SyslogTCPHandler)
+        server.serve_forever(poll_interval=0.5)
+    except (IOError, SystemExit):
+        raise
+    except KeyboardInterrupt:
+        print ("Crtl+C Pressed. Shutting down.")
