e8251e7ff39c907d9dc153a31251c542716e03a6
Jasper <japsai@gmail.com>
2017-06-07 02:15:37 +0100

    Configure() will install basic nginx rsyslog config and restart rsyslog.

--- scrolls/configure.py
+++ scrolls/configure.py
@@ -1,3 +1,4 @@
+import subprocess
 from copy import copy
 
 
@@ -5,9 +6,10 @@ def run():
     ## UDP /etc/rsyslog.conf
     uncomment('/etc/rsyslog.conf', ['$ModLoad imudp', '$UDPServerRun 514'])
     ## nginx
-
-
+    with open('/etc/rsyslog.d/41-nginx.conf', 'w') as fh:
+        fh.write(NGINX)
     ## sudo service rsyslog restart
+    subprocess.check_call(['service', 'rsyslog', 'restart'])
 
 def uncomment(fpath, statements):
     found = {s:False for s in statements}
@@ -31,6 +33,37 @@ def uncomment(fpath, statements):
         print('saving edited file')
         with open(fpath, 'w') as fh:
             fh.writelines(newlines)
+
+NGINX = """
+$ModLoad imfile
+$InputFilePollInterval 10
+$PrivDropToGroup adm
+$WorkDirectory /var/spool/rsyslog
+
+# Nginx access file:
+$InputFileName /var/log/nginx/access.log
+$InputFileTag nginx-access:
+$InputFileStateFile stat-nginx-access
+$InputFileSeverity info
+$InputFilePersistStateInterval 20000
+$InputRunFileMonitor
+
+#Nginx Error file:
+$InputFileName /var/log/nginx/error.log
+$InputFileTag nginx-error:
+$InputFileStateFile stat-nginx-error
+$InputFileSeverity error
+$InputFilePersistStateInterval 20000
+$InputRunFileMonitor
+
+#Add a tag for nginx events
+$template ScrollsFormatNginx,"<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [tag=\"nginx\"] %msg%\n"
+
+if $programname == 'nginx-access' then @@0.0.0.0:514;ScrollsFormatNginx
+if $programname == 'nginx-access' then ~
+if $programname == 'nginx-error' then @@0.0.0.0:514;ScrollsFormatNginx
+if $programname == 'nginx-error' then ~
+"""
 
 
 if __name__ == '__main__':
