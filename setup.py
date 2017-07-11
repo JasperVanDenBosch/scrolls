1a3fa96faa34b7f62564c6cdd9092f3bc9aaeec7
Jasper <japsai@gmail.com>
2017-07-11 15:11:52 +0100

    [style] fixed pep8 in setup.py

--- setup.py
+++ setup.py
@@ -7,23 +7,24 @@ requires = []
 with open('requirements.txt') as f:
     requires = f.read().splitlines()
 
-setup(name='scrolls',
-      version='0.0.'+str(seconds),
-      description='Log manager.',
-      url='https://github.com/ilogue/scrolls',
-      long_description='',
-      classifiers=[
+setup(
+    name='scrolls',
+    version='0.0.'+str(seconds),
+    description='Log manager.',
+    url='https://github.com/ilogue/scrolls',
+    long_description='',
+    classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='Jasper J.F. van den Bosch',
-      author_email='japsai@gmail.com',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      test_suite="test",
-      entry_points={'console_scripts': ['scrolls=scrolls.entrypoint:main']}
+    ],
+    author='Jasper J.F. van den Bosch',
+    author_email='japsai@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    test_suite="test",
+    entry_points={'console_scripts': ['scrolls=scrolls.entrypoint:main']}
 )
