9dedcdc3b9321162fb5fbbecce777bb1ceb427d4
Jasper <japsai@gmail.com>
2017-07-18 01:32:14 +0100

    Message can extract its datetime and convert itself to a tuple.

--- scrolls/models/message.py
+++ scrolls/models/message.py
@@ -1,6 +1,19 @@
+import dateutil.parser
+import time
 
 
 class Message(object):
 
     def __init__(self, client, data):
-        pass
+        self.client = client
+        self.data = data
+
+    def getDatetime(self):
+        s = self.data.split()[1]
+        return dateutil.parser.parse(s)
+
+    def getTimestamp(self):
+        return self.getDatetime().timestamp()
+
+    def toTuple(self):
+        return self.getTimestamp(), self.client, self.data
--- test/message_tests.py
+++ test/message_tests.py
@@ -1,10 +1,35 @@
 from test.ditestcase import DITestCase
+from datetime import datetime
+from mock import Mock
+from dateutil.tz import tzoffset
+import time
 
 
 class MessageTests(DITestCase):
-    pass
 
+    def test_dt_syslog(self):
+        from scrolls.models.message import Message
+        data = '<54>1 2017-01-11T14:44:37.992647+01:00 - - - - -  hallo\n'
+        dt = datetime(2017, 1, 11, 14, 44, 37, 992647,
+                      tzinfo=tzoffset(None, 3600))
+        message = Message('client', data)
+        self.assertEqual(message.getDatetime(), dt)
 
-# def sortkey(m):
-#     s = m[1].split()[1]
-#     return dateutil.parser.parse(s)
+    def test_timestamp(self):
+        from scrolls.models.message import Message
+        dt = datetime(2017, 1, 11, 14, 44, 37, 992647,
+                      tzinfo=tzoffset(None, 3600))
+        message = Message('client', 'data')
+        message.getDatetime = Mock()
+        message.getDatetime.return_value = dt
+        self.assertEqual(message.getTimestamp(), dt.timestamp())
+
+    def test_toTuple(self):
+        from scrolls.models.message import Message
+        message = Message('client', 'data')
+        message.getTimestamp = Mock()
+        message.getTimestamp.return_value = 123.456
+        self.assertEqual(message.toTuple(), (
+            123.456, 'client', 'data'
+        ))
+
