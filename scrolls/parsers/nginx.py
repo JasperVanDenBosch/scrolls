8ab693269a1d8ca3d086b282507dfc4deac7f3c6
Jasper <japsai@gmail.com>
2017-07-24 19:42:43 +0100

    Implemented NginxParser

--- scrolls/parsers/nginx.py
+++ scrolls/parsers/nginx.py
@@ -6,4 +6,14 @@ class NginxParser(object):
         pass
 
     def parse(self, string):
-        return {}
+        strparts = string.split('"')
+        part1, reqpart, statuspart, _, _, uapart, _ = strparts
+        reqparts = reqpart.split()
+        mdict = {
+            'ip': part1.split()[0],
+            'httpcode': int(statuspart.split()[0]),
+            'httpmethod': reqparts[0],
+            'path': reqparts[1],
+            'content': reqparts[1],
+        }
+        return mdict
--- test/nginxparser_tests.py
+++ test/nginxparser_tests.py
@@ -1,10 +1,16 @@
 from test.ditestcase import DITestCase
 
+NGMSG = ('127.0.0.1 - - [22/Jul/2017:16:44:46 +0100] "GET /bla' +
+         ' HTTP/1.1" 404 209 "-" "UAString"\n')
+
 
 class NginxParserTests(DITestCase):
 
-    def test_data_string(self):
-        pass
-
-    def test_dt_syslog(self):
-        pass
+    def test_nginx(self):
+        from scrolls.parsers.nginx import NginxParser
+        mdict = NginxParser(self.dependencies).parse(NGMSG)
+        self.assertEqual(mdict['httpcode'], 404)
+        self.assertEqual(mdict['httpmethod'], 'GET')
+        self.assertEqual(mdict['path'], '/bla')
+        self.assertEqual(mdict['ip'], '127.0.0.1')
+        self.assertEqual(mdict['content'], '/bla')
