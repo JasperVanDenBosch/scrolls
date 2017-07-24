a3b422c210cc1be994a41cfd1d31b78580039c58
Jasper <japsai@gmail.com>
2017-07-24 17:50:06 +0100

    adapted message repository to new factory method

--- test/messagerepository_tests.py
+++ test/messagerepository_tests.py
@@ -7,8 +7,8 @@ class MessageRepositoryTests(DITestCase):
     def test_add_sorts_messages(self):
         from scrolls.repositories.message import MessageRepository
         self.filesys.readJson.return_value = [
-            (1, 'a msg'),
-            (3, 'a msg'),
+            (1, 'a msg', 'mid'),
+            (3, 'a msg', 'mid'),
         ]
         messages = MessageRepository(self.dependencies)
         messages.add([
@@ -17,15 +17,15 @@ class MessageRepositoryTests(DITestCase):
         ])
         saved = self.filesys.writeJson.call_args[0][1]
         self.assertEqual([
-            (1, 'a msg'),
-            (2, 'a msg'),
-            (3, 'a msg'),
-            (4, 'a msg')
+            (1, 'a msg', 'mid'),
+            (2, 'a msg', 'mid'),
+            (3, 'a msg', 'mid'),
+            (4, 'a msg', 'mid'),
         ], saved)
 
     def test_add_limits_database_to_1000_records(self):
         from scrolls.repositories.message import MessageRepository
-        self.filesys.readJson.return_value = [(888, 'a msg')] * 4500
+        self.filesys.readJson.return_value = [(888, 'm', 'i')] * 4500
         messages = MessageRepository(self.dependencies)
         messages.add([self.messageWithTimestamp(999)] * 600)
         saved = self.filesys.writeJson.call_args[0][1]
@@ -33,29 +33,35 @@ class MessageRepositoryTests(DITestCase):
 
     def test_getLatest(self):
         from scrolls.repositories.message import MessageRepository
-        self.message.fromTuple.side_effect = lambda t: ('M', t[0], t[1])
+        self.message.parseFrom.side_effect = lambda t, withId: ('M', t, withId)
         messages = MessageRepository(self.dependencies)
-        self.filesys.readJson.return_value = [(n, str(n)) for n in range(10)]
+        self.filesys.readJson.return_value = [
+            (n, str(n), 'i'+str(n)) for n in range(10)
+        ]
         self.assertEqual(messages.getLatest(filter=Mock(), n=3), [
-            ('M', 7, '7'),
-            ('M', 8, '8'),
-            ('M', 9, '9')
+            ('M', '7', 'i7'),
+            ('M', '8', 'i8'),
+            ('M', '9', 'i9')
         ])
 
     def test_getLatest_uses_filter(self):
         from scrolls.repositories.message import MessageRepository
-        self.message.fromTuple.side_effect = lambda t: ('M', t[0], t[1])
+        self.message.parseFrom.side_effect = lambda t, withId: ('M', t, withId)
         messages = MessageRepository(self.dependencies)
-        self.filesys.readJson.return_value = [(n, str(n)) for n in range(10)]
+        self.filesys.readJson.return_value = [
+            (n, str(n), 'i'+str(n)) for n in range(10)
+        ]
         filter = Mock()
-        filter.accepts.side_effect = lambda m: (m[1] % 2 == 0)
+        filter.accepts.side_effect = lambda m: (int(m[1]) % 2 == 0)
         self.assertEqual(messages.getLatest(filter=filter, n=3), [
-            ('M', 4, '4'),
-            ('M', 6, '6'),
-            ('M', 8, '8')
+            ('M', '4', 'i4'),
+            ('M', '6', 'i6'),
+            ('M', '8', 'i8')
         ])
 
     def messageWithTimestamp(self, ts):
         msg = Mock()
-        msg.toTuple.return_value = (ts, 'a msg')
+        msg.getDatetime().timestamp.return_value = ts
+        msg.getId.return_value = 'mid'
+        msg.getData.return_value = 'a msg'
         return msg
